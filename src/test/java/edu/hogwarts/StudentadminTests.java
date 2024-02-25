36e0412c6442b28b5c764687a80ab92d2777a12e
MadsFolkmann <mads1238@gmail.com>
2024-02-25 23:56:28 +0100

    Part 4 Done

--- src/test/java/edu/hogwarts/StudentadminTests.java
+++ src/test/java/edu/hogwarts/StudentadminTests.java
@@ -1,7 +1,10 @@
 package edu.hogwarts;
 
+import edu.hogwarts.models.Course;
+import edu.hogwarts.models.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class StudentadminTests {
@@ -10,4 +13,17 @@ class StudentadminTests {
     void contextLoads() {
     }
 
+    @Test
+    void testCourseSchoolYear() {
+        Course course = new Course();
+        course.setSchoolYear(2023);
+        assertEquals(2023, course.getSchoolYear());
+    }
+
+    @Test
+    void testStudentSchoolYear() {
+        Student student = new Student();
+        student.setSchoolYear(2023);
+        assertEquals(2023, student.getSchoolYear());
+    }
 }
